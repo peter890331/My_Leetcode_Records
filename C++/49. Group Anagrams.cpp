8cbf0a995770c26aa1010acd49b72355ee653b77
Peter Yu <peter890331@gmail.com>
2023-11-30 19:44:30 +0800

    2023/11/30

--- C++/49. Group Anagrams.cpp
+++ C++/49. Group Anagrams.cpp
@@ -1,16 +1,16 @@
 class Solution {
-public
-    vectorvectorstring groupAnagrams(vectorstring& strs) {
-        unordered_map  string, vectorstring str_mp;
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map  <string, vector<string>> str_mp;
 
-        for (int i=0; istrs.size(); i++){
+        for (int i=0; i<strs.size(); i++){
             string sorted_strs = strs[i];
             sort(sorted_strs.begin(), sorted_strs.end());
             str_mp[sorted_strs].push_back(strs[i]);
         }
-        vector vectorstring ans;
-        for (const auto& [key, values]  str_mp){   
-             //const auto& 表示一個對象的常量引用。這樣的使用方式通常用於迴圈遍歷，確保不會對集合中的元素進行修改，同時保持了對原始數據的效率，因為不需要進行值的拷貝。
+        vector <vector<string>> ans;
+        for (const auto& [key, values] : str_mp){   
+            // const auto& 表示一個對象的常量引用。這樣的使用方式通常用於迴圈遍歷，確保不會對集合中的元素進行修改，同時保持了對原始數據的效率，因為不需要進行值的拷貝。
             ans.push_back(values);
         }
         return ans;
--- Python/1. Two Sum.py
+++ Python/1. Two Sum.py
@@ -6,7 +6,8 @@ class Solution(object):
         :rtype: List[int]
         """
         for i_index, i in enumerate(nums):
-            remain_nums = nums[i_index+1:]
-            t = target-i
+            remain_nums = nums[i_index + 1:]
+            t = target - i
             if t in remain_nums:
-                return [i_index, remain_nums.index(t)+i_index+1]
+                return [i_index, remain_nums.index(t) + i_index + 1]
+
