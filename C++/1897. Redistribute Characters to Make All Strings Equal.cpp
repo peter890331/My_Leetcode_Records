09b47a417e781fc9b5ab91c618cf73f829b73bc1
Peter Yu <peter890331@gmail.com>
2023-12-30 23:45:03 +0800

    2023/12/30 - Temp

---
+++ C++/1897. Redistribute Characters to Make All Strings Equal.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool makeEqual(vector<string>& words) {
+        if (words.size() == 1) {
+            return true;
+        }
+
+        int totalCharCount = 0;
+        for (const string& s : words) {
+            totalCharCount += s.length();
+        }
+
+        if (totalCharCount % words.size() != 0) {
+            return false;
+        }
+
+        vector<int> myMap(26, 0);
+        for (const string& s : words) {
+            for (char c : s) {
+                myMap[c - 'a']++;
+            }
+        }
+
+        for (int i : myMap) {
+            if (i % words.size() != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
+
+// lancertech6's solution.
+// No time to watch yet. 
