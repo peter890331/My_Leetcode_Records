466cecbe533f29d877a8d9dc9676a7d8c5a04cc9
Peter Yu <peter890331@gmail.com>
2024-01-07 00:05:07 +0800

    2024/01/06

---
+++ C++/70. Climbing Stairs.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int result = 0;
+        if (n==0 || n==1){
+            return 1;
+        }
+        vector<int> dp(n+1);
+        dp[0] = dp[1] = 1;
+        for (int i=2; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+};
+
+// Reference from rahulvarma5297's solution.
+// Time Limit Exceeded if used recursion.
